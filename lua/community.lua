0f0c96eb458f90c31e026c9ec3a03d129d73aa43
陈舒豪 <chenshuhao@chenshuhaos-Mac-mini.local>
2024-10-24 14:41:00 +0800

    完善dap配置

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,11 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.colorscheme.catppuccin" }
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.tabout-nvim" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.conform-nvim" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
 }
