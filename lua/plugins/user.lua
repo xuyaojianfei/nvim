601a37173bec94f96fc868bbe5549eea67edc825
陈舒豪 <chenshuhao@chenshuhaos-Mac-mini.local>
2025-12-31 10:54:21 +0800

    添加move插件快捷键

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -18,5 +18,22 @@ return {
   -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = true },
 
-   'fedepujol/move.nvim',
+   {
+    'fedepujol/move.nvim',
+    keys = {
+      -- Normal Mode
+      { "<leader>mj", ":MoveLine(1)<CR>", desc = "Move Line Up" },
+      { "<leader>mk", ":MoveLine(-1)<CR>", desc = "Move Line Down" },
+      { "<leader>mh", ":MoveHChar(-1)<CR>", desc = "Move Character Left" },
+      { "<leader>ml", ":MoveHChar(1)<CR>", desc = "Move Character Right" },
+      { "<leader>mf", ":MoveWord(-1)<CR>", mode = { "n" }, desc = "Move Word Left" },
+      { "<leader>mb", ":MoveWord(1)<CR>", mode = { "n" }, desc = "Move Word Right" },
+      -- Visual Mode
+      { "<leader>mj", ":MoveBlock(1)<CR>", mode = { "v" }, desc = "Move Block Up" },
+      { "<leader>mk", ":MoveHBlock(-1)<CR>", mode = { "v" }, desc = "Move Block Left" },
+      { "<leader>mh", ":MoveBlock(-1)<CR>", mode = { "v" }, desc = "Move Block Down" },
+      { "<leader>ml", ":MoveHBlock(1)<CR>", mode = { "v" }, desc = "Move Block Right" },
+    },
+    opts={},
+  },
 }
